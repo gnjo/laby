b9a4f0b57b80778c97c21bea64e1e18a540fa3b1
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-01-29 17:21:58 +0900

    Update xors.js

--- xors.js
+++ xors.js
@@ -20,6 +20,7 @@ console.log(a)
 5: 427485698
 6: 281552588 
  */
+ /*
  function entry(_s){
   let is=(d)=>{return(d||d===0)?true:false},_seed=_s||371973300,x=472613715,y=413900530,z=942798599,w=_seed,wk
   ,next=()=>{return wk=x^(x<<11),x=y,y=z,z=w, w=(w ^ (w >>> 19)) ^ (wk ^ (wk >>> 8))}
@@ -29,6 +30,16 @@ console.log(a)
     return (!is(max))?next():min + (Math.abs(next()) % (max + 1 - min));
   }
  }
+ */
+ function entry(_s){
+  let _seed=_s||371973300,x=472613715,y=413900530,z=942798599,w=_seed,wk
+  ;
+  return function random(min,max){
+    if((min||min===0)&&!(max||max===0)) max=min,min=0;
+    ;wk=x^(x<<11),x=y,y=z,z=w, w=(w ^ (w >>> 19)) ^ (wk ^ (wk >>> 8));
+    return (!(max||max===0))?w:min + (Math.abs(w) % (max + 1 - min));
+  }
+ } 
  root.xrand=entry
  root.shuffle=shuffle
 })(this);
