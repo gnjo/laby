2160243d59c4f74221185cb73d0c2274943693e8
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-01-29 14:35:00 +0900

    Update xors.js

--- xors.js
+++ xors.js
@@ -10,8 +10,18 @@ let random=xrand(seed)
 let a=Array.from({length:100}).map(d=>random(-100,100))
 console.log(a)
  */
+ /*
+[0,1,2,3,4,5,6].map(d=>Math.floor(Math.random()*1000000000)) 
+0: 371973300
+1: 472613715
+2: 413900530
+3: 942798599
+4: 431705231
+5: 427485698
+6: 281552588 
+ */
  function entry(_s){
-  let is=(d)=>{return(d||d===0)?true:false},_seed=_s||123456789,x=158027586,y=206422233,z=758680801,w=_seed,wk
+  let is=(d)=>{return(d||d===0)?true:false},_seed=_s||371973300,x=472613715,y=413900530,z=942798599,w=_seed,wk
   ,next=()=>{return wk=x^(x<<11),x=y,y=z,z=w, w=(w ^ (w >>> 19)) ^ (wk ^ (wk >>> 8))}
   ;
   return function random(min,max){
